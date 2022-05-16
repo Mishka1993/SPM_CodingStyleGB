95ddb7f6255b492598ab7e6acc41a6267500b176
Mikhail <misha220993@gmail.com>
2022-05-16 14:26:52 +0400

    Lesson_5

---
+++ SPM_CodingStyle/SPM_CodingStyle.h
@@ -0,0 +1,18 @@
+//
+//  SPM_CodingStyle.h
+//  SPM_CodingStyle
+//
+//  Created by Михаил Киржнер on 16.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPM_CodingStyle.
+FOUNDATION_EXPORT double SPM_CodingStyleVersionNumber;
+
+//! Project version string for SPM_CodingStyle.
+FOUNDATION_EXPORT const unsigned char SPM_CodingStyleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPM_CodingStyle/PublicHeader.h>
+
+
---
+++ SPM_CodingStyle/SPM_CodingStyle.swift
@@ -0,0 +1,62 @@
+//
+//  SPM_CodingStyle.swift
+//  SPM_CodingStyle
+//
+//  Created by Михаил Киржнер on 16.05.2022.
+//
+
+import Foundation
+
+public enum CodingCase {
+    case camelCase
+    case snakeCase
+    case kebabCase
+}
+
+@propertyWrapper
+public struct CodingStyle {
+    public private(set) var text: String
+    private var currentCodingCase: CodingCase
+
+    init(wrappedValue: String, codingCase: CodingCase) {
+        text = wrappedValue
+        currentCodingCase = codingCase
+    }
+
+    public var wrappedValue: String {
+        get {
+            get()
+        }
+        set {
+            set(newValue)
+        }
+    }
+
+    private func get() -> String {
+        switch currentCodingCase {
+        case .camelCase:
+            return toCamelCase()
+        case .snakeCase:
+            return toSnakeCase()
+        case .kebabCase:
+            return toKebabCase()
+        }
+    }
+
+    private mutating func set(_ newValue: String) {
+        text = newValue
+    }
+
+    private func toCamelCase() -> String {
+        return text.upperCamelCased
+    }
+
+    private func toSnakeCase() -> String {
+        return text.replacingOccurrences(of: " ", with: "_")
+    }
+
+    private func toKebabCase() -> String {
+        return text.replacingOccurrences(of: " ", with: "-")
+    }
+}
+
